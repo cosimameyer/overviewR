bc26b9330d2f263b76264af3c632bcd8a8887cb0
Cosima Meyer <cosima.meyer@gmail.com>
2022-01-03 17:19:13 +0100

    Update

--- R/overview_tab.R
+++ R/overview_tab.R
@@ -14,7 +14,6 @@
 #' output_table <- overview_tab(dat = toydata, id = ccode, time = year)
 #' @export
 #' @importFrom dplyr "%>%"
-#' @importFrom data.table ".."
 
 overview_tab <- function(dat,
                          id,
