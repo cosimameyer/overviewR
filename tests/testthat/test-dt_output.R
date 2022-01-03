b2ab0a0bd61abe5e83946a9b88937483175df18c
Cosima Meyer <cosima.meyer@gmail.com>
2022-01-03 11:42:34 +0100

    Update

--- tests/testthat/test-dt_output.R
+++ tests/testthat/test-dt_output.R
@@ -1,6 +1,6 @@
 test_that("overview_tab() returns same results regardless of using data.table or data.frame", {
   output_table <- overview_tab(dat = toydata, id = ccode, time = year)
-  toydata_dt <- setDT(toydata)
+  toydata_dt <- data.table::setDT(toydata)
   output_table2 <- overview_tab(dat = toydata_dt, id = ccode, time = year)
   expect_equal(nrow(output_table2), nrow(output_table))
 })
