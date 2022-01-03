8710201c5618e89071fc546c7a80b75f1b8027e6
Cosima Meyer <cosima.meyer@gmail.com>
2022-01-03 14:14:26 +0100

    Added test

--- tests/testthat/test-dt_output.R
+++ tests/testthat/test-dt_output.R
@@ -5,3 +5,12 @@ test_that("overview_tab() returns same results regardless of using data.table or
   output_table2 <- overview_tab(dat = toydata_dt, id = ccode, time = year)
   expect_equal(nrow(output_table2), nrow(output_table))
 })
+
+test_that("overview_na() returns same results regardless of using data.table or data.frame", {
+  output <- overview_na(dat = toydata)
+  toydata <- copy(toydata)
+  toydata_dt <- data.table::setDT(toydata)
+  output2 <- overview_na(dat = toydata_dt)
+  testthat::expect_is(output2, "ggplot")
+  testthat::expect_is(output, "ggplot")
+})
