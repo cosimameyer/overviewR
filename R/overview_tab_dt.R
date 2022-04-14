c89e860cc0105f409cde23d055b53319fad1f543
cosima <cosima.meyer@gmail.com>
2022-04-14 16:08:58 +0200

    Fixed small bug

--- R/overview_tab_dt.R
+++ R/overview_tab_dt.R
@@ -2,7 +2,6 @@
 #'
 #' @description Internal function that calculates the `overview_tab` for data.table objects
 #'
-#' @param dat2 Your data set
 #' @param dat Your data set
 #' @param id Scope (e.g., country codes or individual IDs)
 #' @param time Time (e.g., time periods given by years, months, ...). There are three options to add a date variable: 1) Time can be a character vector containing **one** time variable, 2) a time variable following the YYYY-MM-DD format, or 3) or a list containing multiple time variables (`time = list(year = NULL, month = NULL, day = NULL)`).
